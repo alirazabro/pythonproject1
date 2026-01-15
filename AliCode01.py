fb11bcfa3b15dd1413eb4d319b6dcd42b1c0a812
alirazabro <janniguru51@gmail.com>
2026-01-15 22:08:27 +0500

    Update AliCode01.py

--- AliCode01.py
+++ AliCode01.py
@@ -1,20 +1,25 @@
-# Program to calculate area of circle.square and rectangle
+# Program to calculate area of circle.square and traingle
 import math
+#circle ka area
 def area_circle(radius):
     return math.pi * radius * radius
+#Square ka area
 def area_square(side):
     return side * side
-def area_rectangle(length, width):
-    return length  * width
+#Traingle ka area
+def area_traingle(base, height):
+    return 0.5 * base * height
+#========User Input========
 r = float(input("Enter radius of circle: "))
 s = float(input("Enter side of square: "))
-l = float(input("Enter length of rectangle: "))
-w = float(input("Enter width of rectangle: "))
-
+b = float(input("Enter base of traingle: "))
+h = float(input("Enter height of traingle: "))
+#=======calculation======
 circle_area = area_circle(r)
 square_area = area_square(s)
-rectangle_area = area_rectangle(l,w)
-
+traingle_area = area_traingle(b, h)
+#=======Output=======
 print("Area of cirlce is:",circle_area)
 print("Area of square is:",square_area)
-print("Area of Rectangle:",rectangle_area)
+print("Area of Rectangle:",traingle_area)
+
